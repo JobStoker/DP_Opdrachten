bf97e718f48bd31d97bb343e1058a67b9786dc9f
JobStoker <job.stoker@gmail.com>
2019-04-25 21:40:39 +0200

    x

---
+++ src/p1/Reiziger.java
@@ -0,0 +1,27 @@
+package p1;
+
+import java.util.Date;
+
+public class Reiziger {
+	
+	String naam;
+	Date bdatum;
+	
+	public Reiziger() {}
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+	public void setNaam(String nm) {
+		naam = nm;
+	}
+	
+	public Date getGBdatum() {
+		return bdatum; 
+	}
+	
+	public void setGBdatum(Date dtm) {
+		bdatum = dtm;
+	}
+}
